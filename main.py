16e1acc142650ce72a4172ca718756937010011d
BenjaminTutu <136089746+BenjaminTutu@users.noreply.github.com>
2026-02-11 10:21:09 +0000

    Update main.py
    Removed installation instructions from main.py

--- main.py
+++ main.py
@@ -17,19 +17,6 @@ from flask_gravatar import Gravatar
 from dotenv import load_dotenv
 import os
 
-'''
-Make sure the required packages are installed: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from the requirements.txt for this project.
-'''
-
 load_dotenv()
 
 app = Flask(__name__)
